7c6d235137021c58867e2b8647a2e29604f59549
JoaoFigueired0 <vicentejoao@icloud.com>
2026-04-28 13:03:47 -0300

    ajuste de iniciação do servidor

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ import app from './src/app.js';
 import connectDB from './src/config/db.js';
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT);
 
 const startServer = async () => {
   try {
@@ -12,6 +11,7 @@ const startServer = async () => {
     app.listen(PORT, () => {
       console.log(`Servidor rodando na porta ${PORT}`);
     });
+
   } catch (error) {
     console.error("Erro ao iniciar servidor:", error);
   }
